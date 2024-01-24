a0a88833429a84898b52ea6cd93a03564a721a44
akhilprabhu20 <akhilprabhu20@gmail.com>
2024-01-24 18:38:53 +0530

    code.py and practice.txt

---
+++ code.py
@@ -0,0 +1,9 @@
+ihe word DevOps is a combination of the terms development and operations, meant to represent a collaborative or shared approach to the tasks performed by a company's application development and IT operations teams.
+
+In its broadest meaning, DevOps is a philosophy that promotes better communication and collaboration between these teams -- and others -- in an organization. In its most narrow interpretation, DevOps describes the adoption of iterative software development, automation, and programmable infrastructure deployment and maintenance. The term also covers culture changes, such as building trust and cohesion between developers and systems administrators and aligning technological projects to business requirements. DevOps can change the software delivery chain, services, job roles, IT tools and best practices.
+
+While DevOps is not a technology, DevOps environments generally apply common methodologies. These include the following:
+
+continuous integration and c
+
+print("Damon Salvatore")
